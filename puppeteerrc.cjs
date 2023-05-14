4dc306593815879ed277fa5dc804d031da73c969
Abhishek meena <abhi8076meena@gmail.com>
2023-05-14 18:38:45 +0530

    not

--- puppeteerrc.cjs
+++ puppeteerrc.cjs
@@ -6,4 +6,5 @@ const { join } = require('path');
 module.exports = {
     // Changes the cache location for Puppeteer.
     cacheDirectory: join("/tmp/abhi8076-backend", '.cache', 'puppeteer'),
+    // executablePath: "/tmp/abhi8076-backend/.cache/puppeteer/chrome/linux-113.0.5672.63"
 };
